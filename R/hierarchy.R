1aef946f7b5d8200db1c357890a6d3c68c061c1f
jrklasen <jrklasen@gmx.de>
2015-04-09 21:51:51 +0200

    improved examples

--- R/hierarchy.R
+++ R/hierarchy.R
@@ -10,14 +10,13 @@
 #' n <- 100
 #' p <- 150
 #' # x with correlated columns
-#' sigmaChol <- chol(toeplitz((p:1/p)^3), pivot = TRUE)
-#' sigmaChol <- sigmaChol[, order(attr(sigmaChol, "pivot"))]
-#' x <- matrix(rnorm(n * p), nrow = n) %*% sigmaChol
-#' x <- sweep(x, 2, rnorm(p, 10), "+")
+#' corMat <- toeplitz(p:1/p)
+#' corMatQ <- chol(corMat)
+#' x <- matrix(rnorm(n * p), nrow = n) %*% corMatQ
 #' colnames(x) <- paste0("x", 1:p)
 #' # hierarchy
 #' dend <- as.dendrogram(hclust(dist(t(x))))
-#' hier <- hierarchy(dend)
+#' hier <- hierarchy(dend, max.height = 20)
 #' @importFrom parallel mclapply
 #' @export
 hierarchy <- function (x, height, max.height, names) {
