2e852c6e9d0a9383ddb87e9eee3d0cb6f91cbe94
jrklasen <jrklasen@gmx.de>
2016-09-05 11:06:37 +0200

    hit example

--- R/hit.R
+++ R/hit.R
@@ -46,12 +46,12 @@
 #' n <- 80
 #' p <- 82
 #' ## x with correlated columns
-#' corMat <- toeplitz((p:1/p)^5)
+#' corMat <- toeplitz((p:1/p)^2)
 #' corMatQ <- chol(corMat)
 #' x <- matrix(rnorm(n * p), nrow = n) %*% corMatQ
 #' colnames(x) <- paste0("x", 1:p)
 #' ## y
-#' mu <- x[, c(5, 24, 72)] %*% c(3, 1, 2)
+#' mu <- x[, c(5, 10, 72)] %*% c(2, -2, 2)
 #' y <-  rnorm(n, mu)
 #' ## clustering of the clumns of x
 #' hc <- hclust(dist(t(x)))
